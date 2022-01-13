dab5bf1370fe7e2073aa24133336458b42406cf1
sathish1980 <kumar.sathish189@gmail.com>
2022-01-13 08:29:41 +0530

    Added POM in to the framework

--- src/test/java/CommonMethods/ReusableComponenets.java
+++ src/test/java/CommonMethods/ReusableComponenets.java
@@ -1,5 +1,8 @@
 package CommonMethods;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
@@ -7,10 +10,13 @@ import java.util.Date;
 import java.util.NoSuchElementException;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
@@ -37,6 +43,13 @@ public class ReusableComponenets
 		wait1.until(ExpectedConditions.visibilityOfAllElements(elementToWait));
 	}
 	
+	public void Fluentwaitforelementobeclickable(WebDriver driver, WebElement elementToWait)
+	{
+		FluentWait wait1 = new FluentWait(driver);
+		wait1.withTimeout(Duration.ofSeconds(60)).pollingEvery(Duration.ofSeconds(10)).ignoring(NoSuchElementException.class);
+		wait1.until(ExpectedConditions.elementToBeClickable(elementToWait));
+	}
+	
 	public void waitforelementobeclick(WebDriver driver, WebElement elementToWait)
 	{
 		WebDriverWait wait = new WebDriverWait(driver,60);
@@ -45,7 +58,15 @@ public class ReusableComponenets
 	
 	public void click(WebElement elementoClick)
 	{
-		if(elementoClick.isDisplayed()==true &&elementoClick.isEnabled()==true)
+		if(elementoClick.isDisplayed()==true &&elementoClick.isEnabled()==true )
+		{
+			elementoClick.click();
+		}
+		
+	}
+	public void checkBoxclick(WebElement elementoClick)
+	{
+		if(elementoClick.isDisplayed()==true &&elementoClick.isEnabled()==true && elementoClick.isSelected()==false)
 		{
 			elementoClick.click();
 		}
@@ -59,6 +80,14 @@ public class ReusableComponenets
 			elementoenter.clear();
 			elementoenter.sendKeys(chartoeneter);
 		}
+		
+	}
+	
+	public void Tab_ACtions(WebDriver driver,WebElement elementoenter)
+	{
+		 while(!elementoenter.getAttribute("value").equals("")){
+			 elementoenter.sendKeys(Keys.BACK_SPACE);
+		    }
 		
 	}
 	
@@ -81,4 +110,6 @@ public class ReusableComponenets
 		
 		return Destinationfile.toString();
 	}
+	
+	
 }
--- src/test/java/Testcase/SignUP.java
+++ src/test/java/Testcase/SignUP.java
@@ -1,5 +1,7 @@
 package Testcase;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
@@ -8,10 +10,12 @@ import com.relevantcodes.extentreports.LogStatus;
 import Baseclass.DriverClass;
 import CommonMethods.ReusableComponenets;
 import Pages.SignUpPage;
+import Util.ExcelRead;
 import junit.framework.Assert;
 
 public class SignUP extends DriverClass
 {
+	int count=0;
 
 	@BeforeSuite
 	public void launch()
@@ -20,36 +24,43 @@ public class SignUP extends DriverClass
 		urlLaunch();
 	}
 	
-	@Test
-	public void signinwithvalidvalues()
+	@Test(priority=0,dataProvider="Signupuser")
+	public void signinwithvalidvalues(String u_name,String L_name,String Date,String M_Number,String Pass_D,String C_Pass_D,String E_mail_ID)
 	{
 		try
 		{
-			
+		
 		SignUpPage s = new SignUpPage(driver);
 		String ActualText=s.pageTitle();
 		String ExpectedText="Member Enrollment";
 		Assert.assertEquals(ExpectedText, ActualText);
+		Thread.sleep(2000);
 		s.titleSelection();
-		s.firstname();
+		s.firstname(u_name);
+		Thread.sleep(2000);
+		//s.firstname(u_name);
 		test.log(LogStatus.INFO,"firstname entered");
-		s.lastname();
+		s.lastname(L_name);
+		if(count<1)
+		{
 		s.chatboxclick();
+		}
 		test.log(LogStatus.INFO,"laster entered");
-		s.DOB(1990,"May",11);
+		s.DOB(Date);
 		test.log(LogStatus.INFO,"DOB entered");
-		s.contactNumber();
+		s.contactNumber(M_Number);
 		test.log(LogStatus.INFO,"contact number entered");
-		s.passsword();
+		s.passsword(Pass_D);
 		test.log(LogStatus.INFO,"password entered");
-		s.Confirmpasssword();
+		s.Confirmpasssword(C_Pass_D);
 		test.log(LogStatus.INFO,"confirm password entered");
-		s.emailId();
+		s.emailId(E_mail_ID);
 		test.log(LogStatus.INFO,"emailid  entered");
-		
 		s.agreeCheckbox();
 		test.log(LogStatus.INFO,"checkbox clicked");
-		test.log(LogStatus.PASS, "Mandatory values are enetered sucessfully");
+		String scren = ReusableComponenets.takescreenshot(driver);
+		test.log(LogStatus.PASS, "Mandatory values are enetered sucessfully",test.addScreenCapture(scren));
+		count= count+1;
 		}
 		catch(Exception E)
 		{
@@ -61,10 +72,67 @@ public class SignUP extends DriverClass
 		
 	}
 	
+	
+	
+	@Test(priority=1,dataProvider="Signupgreater18")
+	public void signinwithgreaterthan18years(String u_name,String L_name,String Date,String M_Number,String Pass_D,String C_Pass_D,String E_mail_ID)
+	{
+		try
+		{
+		
+		SignUpPage s = new SignUpPage(driver);
+		String ActualText=s.pageTitle();
+		String ExpectedText="Member Enrollment";
+		Assert.assertEquals(ExpectedText, ActualText);
+		Thread.sleep(2000);
+		s.titleSelection();
+		s.firstname(u_name);
+		Thread.sleep(2000);
+		//s.firstname(u_name);
+		test.log(LogStatus.INFO,"firstname entered");
+		s.lastname(L_name);
+		if(count<1)
+		{
+		s.chatboxclick();
+		}
+		test.log(LogStatus.INFO,"laster entered");
+		s.DOB(Date);
+		
+		test.log(LogStatus.INFO,"DOB entered");
+		Assert.assertEquals(s.greaterTheneighteen(),"Age should be more than 18 year");
+		//s.contactNumber(M_Number);
+		String scren = ReusableComponenets.takescreenshot(driver);
+		test.log(LogStatus.PASS, "Mandatory values are enetered sucessfully",test.addScreenCapture(scren));
+		count= count+1;
+		}
+		catch(Exception E)
+		{
+			String scren = ReusableComponenets.takescreenshot(driver);
+			test.log(LogStatus.FAIL, E);
+			test.log(LogStatus.FAIL, test.addScreenCapture(scren));
+			System.out.println(E);
+		}
+		
+	}
+	
+	
+	
+	@DataProvider(name = "Signupuser")
+	public Object[][] exceldata() throws IOException
+	{
+		return ExcelRead.read("username");
+	}
+	
+	@DataProvider(name = "Signupgreater18")
+	public Object[][] Signupgreatereighteen() throws IOException
+	{
+		return ExcelRead.read("invaliddatavalidation");
+	}
+	
 	@AfterSuite
 	public void teardown()
 	{
 		driver.quit();
-		reportclose();
+		report.flush();
 	}
 }
